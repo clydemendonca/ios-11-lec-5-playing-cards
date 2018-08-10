86cad44b285cdbd9814b609d57754a21c731c4fa
Clyde Mendonca <clyde.mendonca01@gmail.com>
2018-08-10 13:54:00 +0530

    Swipe gesture recogniser added

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -8,18 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var deck = PlayingCardDeck()
 
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left, .right]
+            playingCardView.addGestureRecognizer(swipe)
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var deck = PlayingCardDeck()
-        
-        for _ in 1...10 {
-            if let card = deck.draw() {
-                print("\(card)")
-            }
-        }
     }
 
 }
